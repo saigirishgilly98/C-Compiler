83b58f7fe35fd5860fc1d89cbe7df0666d22b38c
Sai Girish <saigirishgilly98@gmail.com>
2020-01-21 00:04:59 +0530

    Create test1.c

---
+++ Lexical Analyser/test1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+	//For loop with valid and invalid strings
+	char str2;
+	int i, s = 0;	
+	char str[5] = "CDP";
+	char str2[5] = "CD_P";
+	for(i=1; i <= 6; i++)
+		printf("%d",i);
+	
+	return 0;
+}
